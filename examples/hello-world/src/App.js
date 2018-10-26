851a4436be5ca275af9be0cce23aaa01b2822580
Artem Arakcheev <artarakcheev@gmail.com>
2018-10-26 00:24:03 -0500

    Make beautiful hello world example (from click-to-speak)

---
+++ examples/hello-world/src/App.js
@@ -0,0 +1,128 @@
+import React, {
+  Component
+} from 'react'
+
+import {
+  Vuics,
+  initVuics,
+  ButtonDefault,
+  Oscilloscope,
+} from '@vuics/vui-react'
+
+import {
+  Segment,
+  Header,
+  Grid,
+  Form,
+  Message,
+  Accordion,
+  Icon,
+} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+initVuics({
+  apiKey: process.env.REACT_APP_VUICS_API_KEY
+})
+
+export default class App extends Component {
+  onData = data => {
+    console.log('intentName:', data.intentName)
+  }
+
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render = () => {
+    const { activeIndex } = this.state
+    return (
+    <div className='login-form'>
+      <style>
+        {`
+          body > div,
+          body > div > div,
+          body > div > div > div.login-form {
+            height: 100%;
+            background-color: mintcream;
+          }
+        `}
+      </style>
+
+      <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h1' color='teal' textAlign='center'>
+            Hello World!
+          </Header>
+          <Form size='large'>
+            <Segment stacked>
+              <p>
+                You could <i>click</i> on the button and say <b>"Hello"</b>.
+              </p>
+
+              <Vuics
+                name='HelloWorld'
+                onConversationData={this.onData}
+                fillStyle='rgb(255,255,255)'
+                lineWidth={1}
+                strokeStyle='pink'
+              >
+                <Oscilloscope
+                  canvasWrapperClassName='canvasWrapper'
+                  canvasClassName='canvas'
+                />
+                <ButtonDefault
+                  className='button'
+                />
+              </Vuics>
+            </Segment>
+          </Form>
+
+          <br/>
+
+          <Accordion styled>
+            <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+              <Icon name='dropdown' />
+              See code
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === 0}>
+              <p>
+                This is a code of the Hello World voice user interface.
+              </p>
+              <Message size='small' floating>
+                <pre style={{ textAlign: 'left' }}>
+                  { JSON.stringify({
+                    name: 'HelloWorld',
+                    intents: [
+                      {
+                        name: 'Hello',
+                        sampleUtterances: [
+                          'Hello',
+                          'Hi',
+                          'Hey'
+                        ],
+                        answer: 'Hello World!'
+                      }
+                    ]
+                  }, null, 2)}
+                </pre>
+              </Message>
+              <p>
+                <b>Try</b> to <i>code</i> it on the&nbsp;
+                <a href="https://vuics.com/builder" target="_blank"><b>Vuics.com</b></a>&nbsp;
+                platform.
+              </p>
+            </Accordion.Content>
+          </Accordion>
+
+        </Grid.Column>
+      </Grid>
+    </div>
+  )
+  }
+}
